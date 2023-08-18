3d2bf137e2fc5764978a47c07e5f235249970d8f
hameedo1 <141840122+hameedo1@users.noreply.github.com>
2023-08-18 18:08:02 -0500

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -3,14 +3,16 @@
 #install java and set environment variable
 sudo amazon-linux-extras install java-openjdk11 -y
 sudo echo 'JAVA_HOME="/usr/lib/jvm/java-11-openjdk-11.0.20.0.8-1.amzn2.0.1.x86_64"' >> /etc/profile
-$ sudo echo 'PATH=$PATH:$JAVA_HOME/bin' >> /etc/profile
-$ source /etc/profile
+sudo echo 'PATH=$PATH:$JAVA_HOME/bin' >> /etc/profile
+source /etc/profile
 
 
 #install maven and set environmentvariable
 sudo yum install maven -y
-export MAVEN_HOME=/usr/share/maven
-export PATH=$PATH:$MAVEN_HOME/bin
+sudo echo "MAVEN_HOME=\"/usr/share/maven\"" >> /etc/profile
+sudo echo "PATH=\$MAVEN_HOME/bin:\$PATH" >> /etc/profile
+source /etc/profile
+
 
 #install Git 
 sudo yum install git -y
@@ -33,3 +35,6 @@ sudo yum -y install terraform
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
+
+
+
