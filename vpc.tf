ebd47d4141695abd92e3de772ace523f9faa0519
hameedo1 <141840122+hameedo1@users.noreply.github.com>
2023-08-18 18:06:36 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -42,8 +42,26 @@ resource "aws_default_security_group" "default-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 8080
     to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    from_port   = 9000
+    to_port     = 9092
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+ingress {
+    from_port   = 8081
+    to_port     = 8082
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
